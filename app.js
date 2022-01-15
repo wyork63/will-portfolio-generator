e8303421639c25275b43592bf0cff45e7d2b593e
wyork <w.york63@gmail.com>
2022-01-14 19:12:22 -0500

    update foreach loop

---
+++ app.js
@@ -0,0 +1,10 @@
+const profileDataArgs = process.argv.slice(2, process.argv.length);
+console.log(profileDataArgs);
+
+const printProfileData = profileDataArr => {
+    profileDataArr.forEach(profileItem =>  console.log(profileItem));
+    };
+  
+  printProfileData(profileDataArgs);
+
+ 
