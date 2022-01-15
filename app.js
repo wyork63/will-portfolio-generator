3de21ffceecc9e06a4462c2a23d8b9d88a5c5459
wyork <w.york63@gmail.com>
2022-01-14 20:06:35 -0500

    generate html file

--- app.js
+++ app.js
@@ -1,15 +1,13 @@
-const profileDataArgs = process.argv.slice(2);
+const fs = require('fs');
+const generatePage = require('./src/page-template.js');
 
-const printProfileData = profileDataArr => {
-  // This...
-  for (let i = 0; i < profileDataArr.length; i += 1) {
-    console.log(profileDataArr[i]);
-  }
+const profileDataArgs = process.argv.slice(2, process.argv.length);
+// changed const name to user 
+const [user, github] = profileDataArgs;
 
-  console.log('================');
 
-  // Is the same as this...
-  profileDataArr.forEach(profileItem => console.log(profileItem));
-};
-
-printProfileData(profileDataArgs);
+  fs.writeFile('./index.html', generatePage(user, github), err => {
+    if (err) throw err;
+  
+    console.log('Portfolio complete! Check out index.html to see the output!');
+  });
