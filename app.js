cbb4cf7418e93558a4f84d0831706daf5fe9131f
wyork <w.york63@gmail.com>
2022-01-15 14:47:20 -0500

    updated

--- app.js
+++ app.js
@@ -29,10 +29,17 @@ return inquirer.prompt([
           }
       },
       {
+        type: 'confirm',
+        name: 'confirmAbout',
+        message: 'Would you like to enter some information about yourself for an "About" section?',
+        default: true
+      },
+      {
         type: 'input',
         name: 'about',
-        message: 'Provide some information about yourself:'
-      }
+        message: 'Provide some information about yourself:',
+        when: ({ confirmAbout }) => confirmAbout
+      },
   ]);
 };
 
