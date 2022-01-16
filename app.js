95f6fcabc01242441b8ba7f0be35def16b28a1ed
wyork <w.york63@gmail.com>
2022-01-16 15:11:01 -0500

    html creates full doc

--- app.js
+++ app.js
@@ -128,11 +128,11 @@ const promptProject = portFolioData => {
   .then(portFolioData => {
     const pageHTML = generatePage(portFolioData);
 
-    // fs.writeFile('./index.html', pageHTML, err => {
-    //   if (err) throw new Error(err);
+    fs.writeFile('./index.html', pageHTML, err => {
+      if (err) throw new Error(err);
 
-    //   console.log('Page created! Check out index.html in this directory to see it!');
-    // });
+      console.log('Page created! Check out index.html in this directory to see it!');
+    });
   });
 
 
